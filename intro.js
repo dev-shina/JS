25fcbbcd710522ed6937cb2d7fedd4ea40463674
Mubarak Lawal <shinaola.lawal@gmail.com>
2022-06-23 16:54:24 +0100

    first commit

---
+++ intro.js
@@ -0,0 +1,8 @@
+//let NAME = ("Mubarak Lawal")
+//let HEIGHT = ("194cm")
+//let COUNTRY = ("NIGERIA")
+
+//console.log(NAME + " " + "is" + " " + HEIGHT + " " + "in" + " " + "height" + " " + "and" + " " + "from" + " " + COUNTRY)
+
+
+alert("Mubarak lawal is 194cm in height and from Nigeria")
